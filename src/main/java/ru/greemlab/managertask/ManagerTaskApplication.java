56dcb11619fce6bde41ecd830cbaf17b72e8e2fe
Greem4 <i@greem4.ru>
2025-02-22 13:08:17 +0300

    project init

---
+++ src/main/java/ru/greemlab/managertask/ManagerTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.greemlab.managertask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/greemlab/managertask/ManagerTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.greemlab.managertask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
